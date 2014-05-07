799ab2e92bbb7d61a556b612bc4289d3ccbad724
Doug Bromley <doug.bromley@gmail.com>
2014-05-07 15:35:55 +0100

    this-ified the damn file to hell and back

--- play.js
+++ play.js
@@ -1,134 +1,133 @@
 var play_state = {
     create: function() {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.add.sprite(0, 0, 'sky');
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.add.sprite(0, 0, 'sky');
 
-        platforms = game.add.group();
-        platforms.enableBody = true;
+        this.platforms = game.add.group();
+        this.platforms.enableBody = true;
 
-        var ground = platforms.create(0, game.world.height - 64, 'ground1');
-        ground.scale.setTo(1, 1);
-        ground.body.immovable = true;
-        ground = platforms.create(128, game.world.height - 64, 'ground1');
-        ground.scale.setTo(1, 1);
-        ground.body.immovable = true;
-        ground = platforms.create((128*2), game.world.height - 64, 'ground1');
-        ground.scale.setTo(1, 1);
-        ground.body.immovable = true;
-        ground = platforms.create((128*3), game.world.height - 64, 'ground1');
-        ground.scale.setTo(1, 1);
-        ground.body.immovable = true;
-        ground = platforms.create((128*4), game.world.height - 64, 'ground1');
-        ground.scale.setTo(1, 1);
-        ground.body.immovable = true;
-        ground = platforms.create((128*5), game.world.height - 64, 'ground1');
-        ground.scale.setTo(1, 1);
-        ground.body.immovable = true;
-        ground = platforms.create((128*6), game.world.height - 64, 'ground1');
-        ground.scale.setTo(1, 1);
-        ground.body.immovable = true;
+        this.ground = this.platforms.create(0, game.world.height - 64, 'ground1');
+        this.ground.scale.setTo(1, 1);
+        this.ground.body.immovable = true;
+        this.ground = this.platforms.create(128, game.world.height - 64, 'ground1');
+        this.ground.scale.setTo(1, 1);
+        this.ground.body.immovable = true;
+        this.ground = this.platforms.create((128*2), game.world.height - 64, 'ground1');
+        this.ground.scale.setTo(1, 1);
+        this.ground.body.immovable = true;
+        this.ground = this.platforms.create((128*3), game.world.height - 64, 'ground1');
+        this.ground.scale.setTo(1, 1);
+        this.ground.body.immovable = true;
+        this.ground = this.platforms.create((128*4), game.world.height - 64, 'ground1');
+        this.ground.scale.setTo(1, 1);
+        this.ground.body.immovable = true;
+        this.ground = this.platforms.create((128*5), game.world.height - 64, 'ground1');
+        this.ground.scale.setTo(1, 1);
+        this.ground.body.immovable = true;
+        this.ground = this.platforms.create((128*6), game.world.height - 64, 'ground1');
+        this.ground.scale.setTo(1, 1);
+        this.ground.body.immovable = true;
 
-
-        var ledge = platforms.create(600, 300, 'platform');
-        ledge.scale.setTo(0.5, 0.5);
-        ledge.body.immovable = true;
-
-
-        ledge = platforms.create((600+60), 300, 'platformr');
-        ledge.scale.setTo(0.5, 0.5);
-        ledge.body.immovable = true;
-
-        ledge = platforms.create((600-60), 300, 'platforml');
-        ledge.scale.setTo(0.5, 0.5);
-        ledge.body.immovable = true;
-
-        var midledge = platforms.create(230, 240, 'platforml');
-        midledge.scale.setTo(0.5, 0.5);
-        midledge.body.immovable = true;
-
-        var midledge = platforms.create(230+60, 240, 'platformr');
-        midledge.scale.setTo(0.5, 0.5);
-        midledge.body.immovable = true;
+        this.ledge = this.platforms.create(600, 300, 'platform');
+        this.ledge.scale.setTo(0.5, 0.5);
+        this.ledge.body.immovable = true;
 
 
-        ledge = platforms.create(100, 400, 'platforml');
-        ledge.scale.setTo(0.5, 0.5);
-        ledge.body.immovable = true;
-        ledge = platforms.create((100+60), 400, 'platformr');
-        ledge.scale.setTo(0.5, 0.5);
-        ledge.body.immovable = true;
+        this.ledge = this.platforms.create((600+60), 300, 'platformr');
+        this.ledge.scale.setTo(0.5, 0.5);
+        this.ledge.body.immovable = true;
+
+        this.ledge = this.platforms.create((600-60), 300, 'platforml');
+        this.ledge.scale.setTo(0.5, 0.5);
+        this.ledge.body.immovable = true;
+
+        this.midledge = this.platforms.create(230, 240, 'platforml');
+        this.midledge.scale.setTo(0.5, 0.5);
+        this.midledge.body.immovable = true;
+
+        this.midledge = this.platforms.create(230+60, 240, 'platformr');
+        this.midledge.scale.setTo(0.5, 0.5);
+        this.midledge.body.immovable = true;
+
+
+        this.ledge = this.platforms.create(100, 400, 'platforml');
+        this.ledge.scale.setTo(0.5, 0.5);
+        this.ledge.body.immovable = true;
+        this.ledge = this.platforms.create((100+60), 400, 'platformr');
+        this.ledge.scale.setTo(0.5, 0.5);
+        this.ledge.body.immovable = true;
 
         // Stars to collect
-        stars = game.add.group();
-        stars.enableBody = true;
+        this.stars = game.add.group();
+        this.stars.enableBody = true;
 
-        starBurst();
+        this.starBurst();
 
         // My player
-        player = game.add.sprite(32, game.world.height - 150, 'dude');
-        game.physics.arcade.enable(player);
+        this.player = game.add.sprite(32, game.world.height - 150, 'dude');
+        this.game.physics.arcade.enable(this.player);
 
         // Giving him some bounce
-        player.body.bounce.y = 0.4;
-        player.body.gravity.y = 300;
-        player.body.collideWorldBounds = true;
+        this.player.body.bounce.y = 0.4;
+        this.player.body.gravity.y = 300;
+        this.player.body.collideWorldBounds = true;
 
         // He's ALIVE (animation)!
         // Note: Lookup flipping sprites
-        player.animations.add('left', [0, 1, 2, 3], 10, true);
-        player.animations.add('right', [5, 6, 7, 8], 10, true);
+        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
 
         // The crazy sphere
-        sphere = game.add.sprite(32, game.world.height - 250, 'sphere')
-        game.physics.enable(sphere, Phaser.Physics.ARCADE);
-        sphere.body.velocity.x = 100;
-        sphere.body.velocity.y = 100;
-        sphere.body.collideWorldBounds = true;
-        sphere.body.bounce.set(1);
+        this.sphere = game.add.sprite(32, game.world.height - 250, 'sphere')
+        this.game.physics.enable(this.sphere, Phaser.Physics.ARCADE);
+        this.sphere.body.velocity.x = 100;
+        this.sphere.body.velocity.y = 100;
+        this.sphere.body.collideWorldBounds = true;
+        this.sphere.body.bounce.set(1);
 
-        cursors = game.input.keyboard.createCursorKeys();
+        this.cursors = game.input.keyboard.createCursorKeys();
 
-        scoreText = game.add.text(16, 16, 'score: 0', {fontSize: '32px', fill: '#000'});
+        this.scoreText = game.add.text(16, 16, 'score: 0', {fontSize: '32px', fill: '#000'});
 
-        music = game.add.audio('awesome');
-        music.play('', 0, 1, true);
+        this.music = game.add.audio('awesome');
+        this.music.play('', 0, 1, true);
     },
 
     update: function() {
-        game.physics.arcade.collide(player, platforms);
+        this.game.physics.arcade.collide(this.player, this.platforms);
 
         // We need this or the stars just go falling through the floor - funny but not useful
-        game.physics.arcade.collide(stars, platforms);
+        this.game.physics.arcade.collide(this.stars, this.platforms);
 
         // Bouncy sphere
-        game.physics.arcade.collide(sphere, platforms);
-        game.physics.arcade.collide(sphere, player, sphereTouch, null, this);
+        this.game.physics.arcade.collide(this.sphere, this.platforms);
+        this.game.physics.arcade.collide(this.sphere, this.player, this.sphereTouch, null, this);
 
 
         // Make the guy stay still when nowts being pressed
-        player.body.velocity.x = 0;
+        this.player.body.velocity.x = 0;
 
         // Enables player to collect stars "collectStar" is the callback function
-        game.physics.arcade.overlap(player, stars, collectStar, null, this);
+        this.game.physics.arcade.overlap(this.player, this.stars, this.collectStar, null, this);
 
         // The sphere will start eating stars after it has been hit twice
-        game.physics.arcade.collide(sphere, stars, eatStar, null, this);
+        this.game.physics.arcade.collide(this.sphere, this.stars, this.eatStar, null, this);
 
         // Sorting out left, right and stop
-        if (cursors.left.isDown){
-            player.body.velocity.x = -150;
-            player.animations.play('left');
-        }else if(cursors.right.isDown){
-            player.body.velocity.x = 150;
-            player.animations.play('right');
+        if (this.cursors.left.isDown){
+            this.player.body.velocity.x = -150;
+            this.player.animations.play('left');
+        }else if(this.cursors.right.isDown){
+            this.player.body.velocity.x = 150;
+            this.player.animations.play('right');
         }else{
-            player.animations.stop();
-            player.frame = 4;
+            this.player.animations.stop();
+            this.player.frame = 4;
         }
 
         // JUMP!
-        if(cursors.up.isDown && player.body.touching.down){
-            player.body.velocity.y = -350;
+        if(this.cursors.up.isDown && this.player.body.touching.down){
+            this.player.body.velocity.y = -350;
         }
 
     },
@@ -137,7 +136,7 @@ var play_state = {
         star.kill();
 
         score++;
-        scoreText.text = 'Score: ' + score;
+        this.scoreText.text = 'Score: ' + score;
     },
 
     eatStar: function(sphere, star){
@@ -145,26 +144,26 @@ var play_state = {
             star.kill();
             console.log('Eating stars?');
             score--;
-            scoreText.text = 'Score: ' + score;
+            this.scoreText.text = 'Score: ' + score;
         }
     },
 
 
     sphereTouch: function(sphere, player){
         sphereHit++;
-        starBurst();
+        this.starBurst();
     },
 
     // STARBURST collision event with bouncy ball
     starBurst: function(){
         // Stars to collect
         for (var i = 0; i < 12; i++) {
-            var star = stars.create(i * 70, 0, 'star');
+            this.star = this.stars.create(i * 70, 0, 'star');
             // Let them fall is a lot easier than placing them so as to avoid being
             // put inside objects I guess?
-            star.body.gravity.y = 300;
+            this.star.body.gravity.y = 300;
 
-            star.body.bounce.y = 0.7 + Math.random() * 0.2;
+            this.star.body.bounce.y = 0.7 + Math.random() * 0.2;
         }
     }
 }
