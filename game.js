cd799bf3551812e8e9c00b274bf485631f8215e2
Doug Bromley <doug.bromley@gmail.com>
2014-05-07 17:28:46 +0100

    re-organising code and adding credits event handler

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gama');
 
 // Game global vartiables
-var platforms;
-var player;
-var cursors;
-var stars;
+//var platforms;
+//var player;
+//ar cursors;
+//var stars;
 var score = 0;
-var sphere;
+//var sphere;
 var sphereHit = 0;
-var music;
+//var music;
 
+game.state.add('load', load_state);
+game.state.add('credits', credits_state);
 game.state.add('menu', menu_state);
-game.state.add('game', game_state);
+game.state.add('play', play_state);
 
 game.state.start('load');
